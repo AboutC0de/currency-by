e6ec25dd4512a122952f88f417485eace0bcd119
About Code <admin@aboutcode.ru>
2020-10-26 23:00:56 +0300

    linter config, and fix linter issues

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('this is home page'),
-      ),
+    return const Scaffold(
+      body: Text('this is home page'),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:currency_by/home_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,6 +6,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:injectable/injectable.dart';
 
 import 'generated/l10n.dart';
+import 'home_widget.dart';
 import 'injection.dart';
 import 'presentation/routes/router.gr.dart' as generated_router;
 
