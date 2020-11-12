1c5aa95807e70f5f07e3847bceadaa64fd5a8d94
About Code <admin@aboutcode.ru>
2020-11-12 21:21:43 +0300

    exchange rate dto in progress

--- lib/infrastructure/exchange_rate/day_dto/one_day_exchange_rate_dto.dart
+++ lib/infrastructure/exchange_rate/day_dto/one_day_exchange_rate_dto.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../../domain/exchange_rate/one_day/one_day_exchange_rate.dart';
 import '../../../utils/date_utils.dart';
 
 part 'one_day_exchange_rate_dto.freezed.dart';
@@ -19,16 +20,20 @@ abstract class OneDayExchangeRateDTO implements _$OneDayExchangeRateDTO {
         DateTime nbDate,
   }) = _OneDayExchangeRateDTO;
 
-  // factory OneDayExchangeRateDTO.fromDomain(OneDayExchangeRate oneDayExchangeRate) {
-  //   return OneDayExchangeRateDTO(
-  //     id: oneDayExchangeRate.id,
-  //     serverTimeStamp: FieldValue.serverTimestamp(),
-  //   );
-  // }
-  //
-  // OneDayExchangeRate toDomain() {
-  //   return OneDayExchangeRate(id: id);
-  // }
+  factory OneDayExchangeRateDTO.fromDomain(
+      OneDayExchangeRate oneDayExchangeRate) {
+    return OneDayExchangeRateDTO(
+      nb: oneDayExchangeRate.nb,
+      nbDate: oneDayExchangeRate.nbDate,
+    );
+  }
+
+  OneDayExchangeRate toDomain() {
+    return OneDayExchangeRate(
+      nb: nb,
+      nbDate: nbDate,
+    );
+  }
 
   factory OneDayExchangeRateDTO.fromJson(Map<String, dynamic> json) =>
       _$OneDayExchangeRateDTOFromJson(json);
