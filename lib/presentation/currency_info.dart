30cfaf5557b911ff2739509931f309f5fa3a0b1e
Andrei Karabach <aboutcode.ru@gmail.com>
2020-12-04 15:47:00 +0300

    provide controller, use library to handle bottom modal

--- lib/presentation/currency_info.dart
+++ lib/presentation/currency_info.dart
@@ -1,9 +1,15 @@
+import 'package:currency_by/presentation/widgets/divider_with_paddings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
+import '../application/currency_exchange_rate/currency_exchange_rate_bloc.dart';
 import '../domain/exchange_rate/exchange_rate.dart';
 import '../infrastructure/currency_source.dart';
+import '../injection.dart';
 import '../utils/constants.dart';
+import 'currency_info_charts.dart';
 
 class CurrencyInfoModal extends StatelessWidget {
   final ExchangeRate exchangeRate;
@@ -21,167 +27,124 @@ class CurrencyInfoModal extends StatelessWidget {
       color: darkGreyColor,
       child: Scaffold(
         backgroundColor: darkGreyColor,
-        appBar: AppBar(
-          titleSpacing: 0,
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          backgroundColor: darkGreyColor,
-          title: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 26,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  children: [
-                    Text(
-                      exchangeRate.currencyCode,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 27,
-                        fontWeight: FontWeight.w900,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(55),
+          child: AppBar(
+            titleSpacing: 0,
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            backgroundColor: darkGreyColor,
+            flexibleSpace: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        children: [
+                          Text(
+                            exchangeRate.currencyCode,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 27,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            exchangeRate.currencyFullName[0].toLowerCase() +
+                                exchangeRate.currencyFullName.substring(1),
+                            style: const TextStyle(
+                              color: greyColor,
+                              fontSize: 17,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      exchangeRate.currencyFullName[0].toLowerCase() +
-                          exchangeRate.currencyFullName.substring(1),
-                      style: const TextStyle(
-                        color: greyColor,
-                        fontSize: 17,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(ctx);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: backgroundGreyColor,
+                          ),
+                          child: const Icon(
+                            Icons.close,
+                            color: greyColor,
+                            size: 25,
+                          ),
+                        ),
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(ctx);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: backgroundGreyColor,
-                    ),
-                    child: const Icon(
-                      Icons.close,
-                      color: greyColor,
-                      size: 25,
-                    ),
+                    ],
                   ),
-                ),
-              ],
+                  const DividerWithPadding(),
+                ],
+              ),
             ),
           ),
         ),
-        body: const Center(child: Text('text')),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: BlocProvider<CurrencyExchangeRateBloc>(
+            create: (BuildContext context) => getIt<CurrencyExchangeRateBloc>()
+              ..getCurrencyExchangeRates(exchangeRate.currencyCode),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: darkGreyColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                  topRight: Radius.circular(25.0),
+                ),
+              ),
+              child: ListView(
+                padding: const EdgeInsets.symmetric(),
+                controller: ModalScrollController.of(ctx),
+                children: [
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: [
+                        Text(
+                          exchangeRate.nb.toStringAsFixed(4),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          source.getDiffSign(exchangeRate),
+                          style: TextStyle(
+                            color: source.getNbDiffColor(exchangeRate),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  CurrencyInfoCharts(
+                    source: source,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
-//     builder: (BuildContext context) {
-//       return BlocProvider<CurrencyExchangeRateBloc>(
-//         create: (BuildContext context) => getIt<CurrencyExchangeRateBloc>()
-//           ..getCurrencyExchangeRates(exchangeRate.currencyCode),
-//         child: Container(
-//           decoration: const BoxDecoration(
-//             color: darkGreyColor,
-//             borderRadius: BorderRadius.only(
-//               topLeft: Radius.circular(25.0),
-//               topRight: Radius.circular(25.0),
-//             ),
-//           ),
-//           child: ListView(
-//             padding: const EdgeInsets.symmetric(
-//               vertical: 16,
-//               horizontal: 26,
-//             ),
-//             children: [
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   Row(
-//                     crossAxisAlignment: CrossAxisAlignment.baseline,
-//                     children: [
-//                       Text(
-//                         exchangeRate.currencyCode,
-//                         style: const TextStyle(
-//                           color: Colors.white,
-//                           fontSize: 27,
-//                           fontWeight: FontWeight.w900,
-//                         ),
-//                       ),
-//                       const SizedBox(
-//                         width: 10,
-//                       ),
-//                       Text(
-//                         exchangeRate.currencyFullName[0].toLowerCase() +
-//                             exchangeRate.currencyFullName.substring(1),
-//                         style: const TextStyle(
-//                           color: greyColor,
-//                           fontSize: 17,
-//                         ),
-//                         overflow: TextOverflow.ellipsis,
-//                       ),
-//                     ],
-//                   ),
-//                   GestureDetector(
-//                     onTap: () {
-//                       Navigator.pop(context);
-//                     },
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         borderRadius: BorderRadius.circular(20),
-//                         color: backgroundGreyColor,
-//                       ),
-//                       child: const Icon(
-//                         Icons.close,
-//                         color: greyColor,
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               const DividerWithPadding(
-//                 padding: 0,
-//               ),
-//               SizedBox(
-//                 height: 30,
-//                 child: Row(
-//                   children: [
-//                     Text(
-//                       exchangeRate.nb.toStringAsFixed(4),
-//                       style: const TextStyle(
-//                         fontSize: 16,
-//                         color: Colors.white,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       width: 5,
-//                     ),
-//                     Text(
-//                       source.getDiffSign(exchangeRate),
-//                       style: TextStyle(
-//                         color: source.getNbDiffColor(exchangeRate),
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//               CurrencyInfoCharts(
-//                 source: source,
-//               ),
-//             ],
-//           ),
-//         ),
-//       );
-//     },
-//   );
-// }
